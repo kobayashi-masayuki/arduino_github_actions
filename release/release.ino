687a35bc775b9697ca4f6ddf05d6be407553a714
kobayashi-masayuki <kobayashi-masayuki@users.noreply.github.com>
2025-05-16 13:12:07 +0000

    chore: sync current_version to v1.5

--- release/release.ino
+++ release/release.ino
@@ -6,7 +6,7 @@
  */
 #include "WiFi.h"
 
-const double current_version = 1.4;  // 現在のバージョン（double型で表現）
+const double current_version = 1.5;  // 現在のバージョン（double型で表現）
 
 void setup() {
   Serial.begin(115200);
